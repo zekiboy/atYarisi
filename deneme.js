a633c2f677b7b04480134e5710d1f6bfcf3aa7cc
Zeki BOY <zekiiboy@gmail.com>
2025-09-29 23:58:15 +0300

    summary-fixedVersion
    desc fixedversion

--- deneme.js
+++ deneme.js
@@ -1,452 +1,130 @@
-var at1=document.getElementById("atbir");
-var at2=document.getElementById("atiki");
-var at3=document.getElementById("atuc");
-var at4=document.getElementById("atdort");
-var at5=document.getElementById("atbes");
-var bitis=document.getElementById("bitis").offsetLeft-85;
-var rdn1=0, rdn2=0, rdn3=0, rdn4=0, rdn5=0;
-var kontrol=false;
+var at1 = document.getElementById("atbir");
+var at2 = document.getElementById("atiki");
+var at3 = document.getElementById("atuc");
+var at4 = document.getElementById("atdort");
+var at5 = document.getElementById("atbes");
+var bitis = document.getElementById("bitis").offsetLeft - 85;
+var rdn1 = 0, rdn2 = 0, rdn3 = 0, rdn4 = 0, rdn5 = 0;
+var kontrol = false;
+var yarisma;
 
+function Baslat() {
+    let ganyan = parseInt(document.getElementById("ganyanKutu").value);
+    let bahisKodu = parseInt(document.getElementById("bahisKoduKutu").value);
 
-
-function Baslat()
-{
-
-    yarisma=setInterval(Yarisma,750);
-
-}
-
-
-
-var Yarisma=function()
-{
-    var ganyan=parseInt(document.getElementById("ganyanKutu").value);
-    var a = document.getElementById("bahisKoduKutu").value;
-    //ganyan a gibi değerleri let ile tanımlarsan farklı metodlarla da kullanabilirsin.
-    if(a<1)
-    {
+    if (isNaN(ganyan) || isNaN(bahisKodu) || bahisKodu < 1) {
         alert("Önce Bahis Yapınız!");
-        return
+        return;
     }
-    else
-    {
-        atbir();
-        atiki();
-        atuc();
-        atdort();
-        atbes();
-        atSirasi();
-    }
+    
+    yarisma = setInterval(Yarisma, 750);
 }
 
+function Yarisma() {
+    let bahisKodu = parseInt(document.getElementById("bahisKoduKutu").value);
+    
+    atYarisi(at1, 'Erbatur', 1.20, bahisKodu, 1);
+    atYarisi(at2, 'Drogba', 1.80, bahisKodu, 2);
+    atYarisi(at3, 'Bob Akif', 1.40, bahisKodu, 3);
+    atYarisi(at4, 'Aboubakar', 2.40, bahisKodu, 4);
+    atYarisi(at5, 'Sergen', 2.70, bahisKodu, 5);
+    atSirasi();
+}
 
-var rastgeleSayiUret= function()
-{
+function rastgeleSayiUret() {
     var rdn;
-    do
-    {
-        rdn=Math.floor(Math.random()*100);
-    }
-    while(rdn<50);
+    do {
+        rdn = Math.floor(Math.random() * 100);
+    } while (rdn < 50);
     return rdn;
 }
 
-/*
-function atbir()
-{
-    if(rdn1<bitis)
-    {
-        rdn1+=rastgeleSayiUret();
-        if(rdn1>=bitis)
-        {
-            rdn1=bitis;
+function guncelle() {
+    var eklenen = parseInt(prompt("YATIRILACAK TUTARI GİRİNİZ:"));
+    var mevcutBakiye = parseInt(document.getElementById("mevcutBakiye").innerHTML);
+    var guncelBakiye = mevcutBakiye + eklenen;
+    document.getElementById("mevcutBakiye").innerHTML = guncelBakiye;
+}
+
+function bahisYap() {
+    let ganyan = parseInt(document.getElementById("ganyanKutu").value);
+    let bahisKodu = parseInt(document.getElementById("bahisKoduKutu").value);
+    var mevcutBakiye = parseInt(document.getElementById("mevcutBakiye").innerHTML) || 0; // Ensure mevcutBakiye is a number
+    var guncelBakiye = mevcutBakiye - ganyan;
+    
+    if (guncelBakiye < 0) {
+        alert("Oynanacak bahis için bakiyeniz yetersiz. Lütfen bakiye güncelleyiniz.");
+        mevcutBakiye = parseInt(document.getElementById("mevcutBakiye").innerHTML) || 0; // Reset mevcutBakiye to avoid NaN
+        return;
+    }
+    
+    document.getElementById("mevcutBakiye").innerHTML = guncelBakiye;
+
+    var atIsimleri = ["Erbatur", "Drogba", "Bob Akif", "Aboubakar", "Sergen"];
+    var oranlar = [1.20, 1.80, 1.40, 2.40, 2.70];
+
+    if (bahisKodu >= 1 && bahisKodu <= 5) {
+        document.getElementById("bahisTutari").innerHTML = "Olası kazanç: " + (oranlar[bahisKodu - 1] * ganyan) + "TL Bahis Alınan At: " + atIsimleri[bahisKodu - 1];
+    }
+}
+
+function atYarisi(at, atIsmi, oran, bahisKodu, atNo) {
+    var rdn = eval("rdn" + atNo);
+    
+    if (rdn < bitis) {
+        rdn += rastgeleSayiUret();
+        if (rdn >= bitis) {
+            rdn = bitis;
             clearInterval(yarisma);
-            if(!kontrol)
-            {
-                kontrol=true;
-               setTimeout(function(){alert("Kazanan at: 1");},2000);
+            if (!kontrol) {
+                kontrol = true;
+                setTimeout(function() {
+                    var ganyan = parseInt(document.getElementById("ganyanKutu").value);
+                    var bahisKodu = parseInt(document.getElementById("bahisKoduKutu").value);
+                    var mevcutBakiye = parseInt(document.getElementById("mevcutBakiye").innerHTML);
+
+                    if (bahisKodu === atNo) {
+                        alert("Kazanan at: " + atNo + " " + atIsmi + " " + (oran * ganyan) + "TL KAZANDINIZ!");
+                        var guncelBakiye = mevcutBakiye + (oran * ganyan);
+                        document.getElementById("mevcutBakiye").innerHTML = guncelBakiye;
+                    } else {
+                        alert("Kazanan at: " + atNo + " " + atIsmi + ". Bahsi Kaybettiniz.");
+                    }
+
+                    atlariSifirla();
+                }, 2000);
             }
-            at1.style.marginLeft=bitis+"px";
+            at.style.marginLeft = bitis + "px";
+        } else {
+            at.style.marginLeft = rdn + "px";
         }
-        else
-        {
-            at1.style.marginLeft=rdn1+"px";
-        }    
     }
+    eval("rdn" + atNo + " = rdn");
 }
 
-function atiki()
-{
-    if(rdn2<bitis)
-    {
-        rdn2+=rastgeleSayiUret();
-        if(rdn2>=bitis)
-        {
-            rdn2=bitis;
-            clearInterval(yarisma);
-            if(!kontrol)
-            {
-                kontrol=true;
-               setTimeout(function(){alert("Kazanan at: 2");},2000);
-            }
-            at2.style.marginLeft=bitis+"px";
-        }
-        else
-        {
-            at2.style.marginLeft=rdn2+"px";
-        }  
-    } 
+function atlariSifirla() {
+    rdn1 = rdn2 = rdn3 = rdn4 = rdn5 = 0;
+    at1.style.marginLeft = at2.style.marginLeft = at3.style.marginLeft = at4.style.marginLeft = at5.style.marginLeft = "0px";
+    kontrol = false;
+
+    // Reset ganyanKutu and bahisKoduKutu values
+    document.getElementById("ganyanKutu").value = "";
+    document.getElementById("bahisKoduKutu").value = "";
+    alert("Yeni bahis yapınız!");
 }
 
-function atuc()
-{
-    if(rdn3<bitis)
-    {
-        rdn3+=rastgeleSayiUret();
-        if(rdn3>=bitis){   
-            rdn3=bitis;
-            clearInterval(yarisma);
-            if(!kontrol)
-            {
-                kontrol=true;
-               setTimeout(function(){alert("Kazanan at: 3");},2000);
-            }
-            at3.style.marginLeft=bitis+"px";
-        }
-        else
-        {
-            at3.style.marginLeft=rdn3+"px";
-        } 
-    }
+function atSirasi() {
+    var siralama = [
+        { at: at1, isim: "Erbatur" },
+        { at: at2, isim: "Drogba" },
+        { at: at3, isim: "Bob Akif" },
+        { at: at4, isim: "Aboubakar" },
+        { at: at5, isim: "Sergen" }
+    ];
+
+    siralama.sort(function(a, b) {
+        return b.at.offsetLeft - a.at.offsetLeft;
+    });
+
+    document.getElementById("siralamaSonucu").innerHTML = siralama[0].isim + " Önde";
 }
-
-function atdort()
-{
-    if(rdn4<bitis)
-    {
-        rdn4+=rastgeleSayiUret();
-        if(rdn4>=bitis)
-        {
-            rdn4=bitis;
-            clearInterval(yarisma);
-            if(!kontrol)
-            {
-                kontrol=true;
-               setTimeout(function(){alert("Kazanan at: 4");},2000);
-            }
-            at4.style.marginLeft=bitis+"px";
-        }
-        else
-        {
-            at4.style.marginLeft=rdn4+"px";
-        }
-    }
-}
-
-function atbes()
-{
-    if(rdn5<bitis)
-    {
-        rdn5+=rastgeleSayiUret();
-        if(rdn5>=bitis)
-        {
-            rdn5=bitis;
-            clearInterval(yarisma);
-            if(!kontrol)
-            {
-                kontrol=true;
-               setTimeout(function(){alert("Kazanan at: 5");},2000);
-            }
-            at5.style.marginLeft=bitis+"px";
-        }
-        else
-        {
-            at5.style.marginLeft=rdn5+"px";
-        }
-    }
-   
-}
-*/
-function guncelle()
-{
-
-    var eklenen=parseInt(prompt("YATIRILACAK TUTARI GİRİNİZ:"));
-    mevcutBakiye=parseInt(document.getElementById("mevcutBakiye").innerHTML);
-    var guncelB=eklenen+mevcutBakiye;
-    document.getElementById("mevcutBakiye").innerHTML=guncelB;
-}
-
-function bahisYap()
-{
-    var ganyan=parseInt(document.getElementById("ganyanKutu").value);
-    var a = document.getElementById("bahisKoduKutu").value;
-
-    mevcutBakiye=parseInt(document.getElementById("mevcutBakiye").innerHTML);
-    var guncelB=mevcutBakiye-ganyan
-    document.getElementById("mevcutBakiye").innerHTML=guncelB;
-
-
-
-
-    if(a==1)
-    {
-        document.getElementById("bahisTutari").innerHTML= "Olası kazanç: "+1.20*ganyan +"TL Bahis Alınan At: Erbatur";
-    }
-    if(a==2)
-    {
-        document.getElementById("bahisTutari").innerHTML= "Olası kazanç: "+1.80*ganyan +"TL Bahis Alınan At: Drogba";
-    }
-    if(a==3)
-    {
-        document.getElementById("bahisTutari").innerHTML= "Olası kazanç: "+1.40*ganyan +"TL Bahis Alınan At: Bob Akif";
-    }
-    if(a==4)
-    {
-        document.getElementById("bahisTutari").innerHTML= "Olası kazanç: "+2.40*ganyan +"TL Bahis Alınan At: Aboubakar";
-    }
-    if(a==5)
-    {
-        document.getElementById("bahisTutari").innerHTML= "Olası kazanç: "+2.70*ganyan +"TL Bahis Alınan At: Sergen";
-    }
-
-    if(guncelB<ganyan)
-    {
-        setTimeout(function(){alert("Oynanacak bahis için bakiyeniz yetersiz. Lütfen bakiye güncelleyiniz.");});
-        var bakiyeYetersiz = guncelB+ganyan
-        document.getElementById("mevcutBakiye").innerHTML=bakiyeYetersiz;
-        document.getElementById("bahisTutari").innerHTML="Bakiye yetersiz olduğu için bahis alınamadı.";
-
-    }
-
-}
-
-function atbir()
-{
-    if(rdn1<bitis)
-    {
-        rdn1+=rastgeleSayiUret();
-        if(rdn1>=bitis)
-        {
-            rdn1=bitis;
-            clearInterval(yarisma);
-            if(!kontrol)
-            {
-                kontrol=true;
-               setTimeout(function()
-               {
-                var ganyan=parseInt(document.getElementById("ganyanKutu").value);
-                var a = document.getElementById("bahisKoduKutu").value;
-                mevcutBakiye=parseInt(document.getElementById("mevcutBakiye").innerHTML);
-
-                if(a==1)
-                {
-                    alert("Kazanan at: 1 Erbatur"+1.20*ganyan+"TL KAZANDINIZ!");
-                    var guncelB=mevcutBakiye+1.20*ganyan;
-                    document.getElementById("mevcutBakiye").innerHTML=guncelB;
-                }
-                else
-                {
-                    alert("Kazanan at: 1 Erbatur. Bahsi Kaybettiniz.");
-                }
-
-               },2000);
-            }
-            at1.style.marginLeft=bitis+"px";
-        }
-        else
-        {
-            at1.style.marginLeft=rdn1+"px";
-        }    
-    }
-}
-
-function atiki()
-{
-    if(rdn2<bitis)
-    {
-        rdn2+=rastgeleSayiUret();
-        if(rdn2>=bitis)
-        {
-            rdn2=bitis;
-            clearInterval(yarisma);
-            if(!kontrol)
-            {
-                kontrol=true;
-               setTimeout(function()
-               {
-                var ganyan=parseInt(document.getElementById("ganyanKutu").value);
-                var a = document.getElementById("bahisKoduKutu").value;
-                mevcutBakiye=parseInt(document.getElementById("mevcutBakiye").innerHTML);
-
-                if(a==2)
-                {
-                    alert("Kazanan at: 2 Drogba"+1.80*ganyan+"TL KAZANDINIZ!");
-                    var guncelB=mevcutBakiye+1.80*ganyan;
-                    document.getElementById("mevcutBakiye").innerHTML=guncelB;
-                }
-                else
-                {
-                    alert("Kazanan at: 2 Drogba. Bahsi Kaybettiniz.");
-                }
-               },2000);
-            }
-            at2.style.marginLeft=bitis+"px";
-        }
-        else
-        {
-            at2.style.marginLeft=rdn2+"px";
-        }  
-    } 
-}
-
-function atuc()
-{
-    if(rdn3<bitis)
-    {
-        rdn3+=rastgeleSayiUret();
-        if(rdn3>=bitis){   
-            rdn3=bitis;
-            clearInterval(yarisma);
-            if(!kontrol)
-            {
-                kontrol=true;
-               setTimeout(function(){
-                var ganyan=parseInt(document.getElementById("ganyanKutu").value);
-                var a = document.getElementById("bahisKoduKutu").value;
-                mevcutBakiye=parseInt(document.getElementById("mevcutBakiye").innerHTML);
-
-                if(a==3)
-                {
-                    alert("Kazanan at: Bob Akif"+1.40*ganyan+"TL KAZANDINIZ!");
-                    var guncelB=mevcutBakiye+1.40*ganyan;
-                    document.getElementById("mevcutBakiye").innerHTML=guncelB;
-                }
-                else
-                {
-                    alert("Kazanan at: 2 Bob Akif. Bahsi Kaybettiniz.");
-                }
-               },2000);
-            }
-            at3.style.marginLeft=bitis+"px";
-        }
-        else
-        {
-            at3.style.marginLeft=rdn3+"px";
-        } 
-    }
-}
-
-function atdort()
-{
-    if(rdn4<bitis)
-    {
-        rdn4+=rastgeleSayiUret();
-        if(rdn4>=bitis)
-        {
-            rdn4=bitis;
-            clearInterval(yarisma);
-            if(!kontrol)
-            {
-                kontrol=true;
-               setTimeout(function(){
-                var ganyan=parseInt(document.getElementById("ganyanKutu").value);
-                var a = document.getElementById("bahisKoduKutu").value;
-                mevcutBakiye=parseInt(document.getElementById("mevcutBakiye").innerHTML);
-
-                if(a==4)
-                {
-                    alert("Kazanan at: 4 Aboubakar"+2.40*ganyan+"TL KAZANDINIZ!");
-                    var guncelB=mevcutBakiye+2.40*ganyan;
-                    document.getElementById("mevcutBakiye").innerHTML=guncelB;
-                }
-                else
-                {
-                    alert("Kazanan at: 4 Aboubakar. Bahsi Kaybettiniz.");
-                }
-               },2000);
-            }
-            at4.style.marginLeft=bitis+"px";
-        }
-        else
-        {
-            at4.style.marginLeft=rdn4+"px";
-        }
-    }
-}
-
-function atbes()
-{
-    if(rdn5<bitis)
-    {
-        rdn5+=rastgeleSayiUret();
-        if(rdn5>=bitis)
-        {
-            rdn5=bitis;
-            clearInterval(yarisma);
-            if(!kontrol)
-            {
-                kontrol=true;
-               setTimeout(function(){
-                var ganyan=parseInt(document.getElementById("ganyanKutu").value);
-                var a = document.getElementById("bahisKoduKutu").value;
-                mevcutBakiye=parseInt(document.getElementById("mevcutBakiye").innerHTML);
-
-                if(a==5)
-                {
-                    alert("Kazanan at: 5 Sergen"+2.70*ganyan+"TL KAZANDINIZ!");
-                    var guncelB=mevcutBakiye+2.70*ganyan;
-                    document.getElementById("mevcutBakiye").innerHTML=guncelB;
-                }
-                else
-                {
-                    alert("Kazanan at: 5 Sergen. Bahsi Kaybettiniz.");
-                }
-               },2000);
-            }
-            at5.style.marginLeft=bitis+"px";
-        }
-        else
-        {
-            at5.style.marginLeft=rdn5+"px";
-        }
-    }
-   
-}
-
-
-// function atSirasi()
-// {
-
-//     atsirasii=setInterval(attSirasi,150);
-
-// }
-
-
-// var attSirasi=function()
- function atSirasi()
-{
-
-    if((at1.offsetLeft>at2.offsetLeft)&&(at1.offsetLeft>at3.offsetLeft)&&(at1.offsetLeft>at4.offsetLeft)&&(at1.offsetLeft>at5.offsetLeft))
-    {
-        document.getElementById("siralamaSonucu").innerHTML="Erbatur Önde";
-    }
-    else if((at2.offsetLeft>at1.offsetLeft)&&(at2.offsetLeft>at3.offsetLeft)&&(at2.offsetLeft>at4.offsetLeft)&&(at2.offsetLeft>at5.offsetLeft))
-    {
-        document.getElementById("siralamaSonucu").innerHTML="Drogba Önde";
-    }
-    else if((at3.offsetLeft>at1.offsetLeft)&&(at3.offsetLeft>at2.offsetLeft)&&(at3.offsetLeft>at4.offsetLeft)&&(at3.offsetLeft>at5.offsetLeft))
-    {
-        document.getElementById("siralamaSonucu").innerHTML="Bob Akif Önde";
-    }
-    else if((at4.offsetLeft>at1.offsetLeft)&&(at4.offsetLeft>at2.offsetLeft)&&(at4.offsetLeft>at3.offsetLeft)&&(at4.offsetLeft>at5.offsetLeft))
-    {
-        document.getElementById("siralamaSonucu").innerHTML="Aboubakar Önde";
-    }
-    else if((at5.offsetLeft>at1.offsetLeft)&&(at5.offsetLeft>at2.offsetLeft)&&(at5.offsetLeft>at3.offsetLeft)&&(at5.offsetLeft>at4.offsetLeft))
-    {
-        document.getElementById("siralamaSonucu").innerHTML="Sergen Önde";
-    }
-    // clearInterval(atsirasii);
-}
-
-
